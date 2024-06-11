502528ece089689ebaa554e950d60699c3e73e13
tsudakanako <167269340+tsudakanako@users.noreply.github.com>
2024-06-11 23:27:45 +0900

    kadai_021をアップロード

---
+++ kadai_021/event.js
@@ -0,0 +1,10 @@
+const text = document.getElementById('text');
+const btn = document.getElementById('btn');
+
+
+btn.addEventListener('click', () => {
+  setTimeout(() => {
+  text.textContent ='ボタンをクリックしました';
+}, 2000);
+
+});
